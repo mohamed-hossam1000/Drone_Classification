a3fe0a01b807d6626fafa64a94057edba820bc8e
Maram Essam <maramessam02@gmail.com>
2026-02-21 14:01:37 +0200

    final model update

--- model.py
+++ model.py
@@ -3,10 +3,6 @@ import torch.nn as nn
 import torchvision.models as models
 
 
-# =========================================
-# 🔵 PART 1 — MODEL ARCHITECTURE (MARAM)
-# =========================================
-
 def build_model(num_classes=3, device="cuda"):
     """
     Build ResNet50 model and replace final layer
@@ -58,14 +54,11 @@ def get_loss_function():
     return nn.CrossEntropyLoss()
 
 
-# # =========================================
-# # 🟢 PART 2 — TRAINING (YOUR FRIEND)
-# # =========================================
 
-# def train_one_epoch(model, dataloader, optimizer, criterion, device):
-#     model.train()
-#     running_loss = 0.0
-#     correct = 0
+def train_one_epoch(model, dataloader, optimizer, criterion, device):
+    model.train()
+    running_loss = 0.0
+    correct = 0
 
     for images, labels in dataloader:
         images = images.to(device)
@@ -113,5 +106,7 @@ def validate(model, dataloader, criterion, device):
     return val_loss, val_acc.item()
 
 
-# def save_model(model, path="resnet50_model.pth"):
-#     torch.save(model.state_dict(), path)
+def save_model(model, path="resnet50_model.pth"):
+    torch.save(model.state_dict(), path)
+    
+print ("Model architecture and training functions defined successfully!")   
